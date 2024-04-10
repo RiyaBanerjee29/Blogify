29c01b25a5ca1abb8b06bb1ee2b7955c13484bdf
“Riya <riyabanerejee.3332@gmail.com>
2024-04-10 22:07:14 +0530

    changes in apikey of TinyMCE

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -5,6 +5,7 @@ const conf = {
     appWriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASEID),
     appWriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appWriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    openAiKey : String(import.meta.env.VITE_OPENAI_API_KEY )
+    openAiKey : String(import.meta.env.VITE_OPENAI_API_KEY ),
+    tinyMCEApiKey : String(import.meta.env.VITE_TINYMCE_API_KEY)
     }
 export default conf
